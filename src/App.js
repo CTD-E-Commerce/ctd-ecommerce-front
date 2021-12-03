5cba634686f69ccb82bec291728192524eae44c0
pcamposaugusto <paulaocaugusto@gmail.com>
2021-12-03 01:48:26 -0300

    add componente Categorias

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.scss';
 import Header from './Components/Header';
+import Categorias from './Components/Galeria/Categorias';
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* <Banner />
+      <Galeria> */}
+        <Categorias />
+        {/* <Carousel />
+      </Galeria>
+      <Sobre />
+      <Footer /> */}
     </div>
   );
 }
--- src/Components/Galeria/Categorias/index.js
+++ src/Components/Galeria/Categorias/index.js
@@ -1,10 +1,53 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './style.scss';
+import makeup from '../../../assets/makeup.png';
+import skinCare from '../../../assets/skin-care.png';
+import hipo from '../../../assets/hipo.png';
+import perfume from '../../../assets/perfume.png';
+import hair from '../../../assets/hair.png';
 
-export const Categorias = () => {
+const Categorias = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="categorias">
+            <Container fluid>
+                <h3 className="h3-categorias">Categorias</h3>
+                <Row>
+                    <Col sm className="item-categorias">
+                        <a href="">
+                            <img src={skinCare} alt="desenho de um tubo de creme" />
+                            <p>Skin care</p>
+                        </a>
+                    </Col>
+                    <Col sm className="item-categorias">
+                        <a href="">
+                            <img src={makeup} alt="desenho de um batom" />
+                            <p>Make up</p>
+                        </a>
+                    </Col>
+                    <Col sm className="item-categorias">
+                        <a href="">
+                            <img src={hair} alt="" />
+                            <p>Hair care</p>
+                        </a>
+                    </Col>
+                    <Col sm className="item-categorias">
+                        <a href="">
+                            <img src={perfume} alt="" />
+                            <p>Perfume</p>
+                        </a>
+                    </Col>
+                    <Col sm className="item-categorias">
+                        <a href="">
+                            <img src={hipo} alt="" />
+                            <p>Hipoalergênico</p>
+                        </a>
+                    </Col>                 
+                </Row>
+            </Container>            
+        </section>
     )
 }
+
+export default Categorias;
 
