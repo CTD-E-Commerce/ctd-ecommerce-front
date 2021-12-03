b29c2b900e14c4cc7f47acad448b82d1857d5c0c
pcamposaugusto <paulaocaugusto@gmail.com>
2021-12-03 16:46:55 -0300

    add o componente sobre

--- src/Components/Sobre/index.js
+++ src/Components/Sobre/index.js
@@ -1,11 +1,86 @@
 import React from 'react';
 import './style.scss';
+import { Container, Row, Col } from 'react-bootstrap';
+import damares from '../../assets/damares.png';
+import felipe from '../../assets/felipe.png';
+import marcelo from '../../assets/marcelo.png';
+import paula from '../../assets/paula.png';
+import pedro from '../../assets/pedro.png';
+import silvano from '../../assets/silvano.png';
+import github from '../../assets/github.svg';
+import linkedin from '../../assets/linkedin.svg';
 
 const Sobre = () => {
     return (
-        <div>
-            
-        </div>
+        <section id="sobre">
+            <Container fluid className="container-sobre">
+                <p>Conheça nossos</p>
+                <h3>Desenvolvedores</h3>
+                <Row className="row-sobre">
+                    <Col sm>
+                        <img className="img-sobre" src={damares} alt=""/>
+                        <p>Dâmares Bortolucci</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/damaresbortolucci" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/damaresbs/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm>
+                        <img className="img-sobre" src={felipe} alt=""/>
+                        <p>Felipe Moreira</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/moreirafelipe" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/felipen20/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm>
+                        <img className="img-sobre" src={marcelo} alt=""/>
+                        <p>Marcelo Miyoshi</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/Marcelomsz" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/marcelo-miyoshi-de-souza-30ba381a4/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm>
+                        <img className="img-sobre" src={paula} alt=""/>
+                        <p>Paula Augusto</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/pcamposaugusto" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/pcamposaugusto/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm>
+                        <img className="img-sobre" src={pedro} alt=""/>
+                        <p>Pedro Brito</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/pedroisb" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/pedrobrito28/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm>
+                        <img className="img-sobre" src={silvano} alt=""/>
+                        <p>Silvano Araujo</p>
+                        <Row>
+                            <Col sm>
+                                <a className="icones" href="https://github.com/Silvanoeng" target="_blanck" ref="noreferrer"><img src={github} alt="" /></a> 
+                                <a className="icones" href="https://www.linkedin.com/in/silvano-araujo/" target="_blanck" ref="noreferrer"><img src={linkedin} alt="" /></a> 
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+
+        </section>
     )
 }
 
