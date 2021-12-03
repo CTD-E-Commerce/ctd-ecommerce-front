e55e23cc40ca35425895216b9ecd5f7dd5e383da
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-12-03 01:35:02 -0300

    add banner e carousel

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.scss';
+import { Banner } from './Components/Banner';
+import { Carrossel } from './Components/Galeria/Carousel';
 import Header from './Components/Header';
+
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Banner />
+      <Carrossel />
     </div>
   );
 }
--- src/Components/Galeria/Carousel/index.js
+++ src/Components/Galeria/Carousel/index.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from 'react';
+import './style.scss';
+import { Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Item } from './item';
 
-export const Carousel = () => {
+
+export const Carrossel = () => {
+
+    /* ARRAY PARA TESTAR A PASSAGEM DE PARAMETROS PARA OS CARDS */
+    const produtos =[
+        {
+            id: 1,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 20,00"
+        },
+        {
+            id: 2,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 200,00"
+        },
+        {
+            id: 3,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 30,00"
+        }
+    ]
+
     return (
         <div>
-            
+            <Carousel>
+                <Carousel.Item>
+                    <div className="container-card">
+                        {produtos.map(({ id, produto, descricao, preco }) => {
+                            return (
+                                <Item id={id} produto={produto} descricao={descricao} preco={preco} />
+                            )
+                        })}
+                    </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className="container-card">
+                        <Item />
+                        <Item />
+                        <Item />
+                    </div>
+                </Carousel.Item>
+            </Carousel>
+
+            {/* CARROSSEL 2 */}
+
+            <Carousel>
+                <Carousel.Item>
+                    <div className="container-card">
+                        <Item />
+                        <Item />
+                        <Item />
+                    </div>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <div className="container-card">
+                        <Item />
+                        <Item />
+                        <Item />
+                    </div>
+                </Carousel.Item>
+            </Carousel>
         </div>
     )
 }
