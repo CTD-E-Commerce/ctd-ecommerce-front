9478914d2667794b6f74861d24563eb9bd2844ab
Silvano AP <silvanoeng@hotmail.com>
2021-12-04 18:52:50 -0300

    Criando logica dos produtos no Carousel

--- src/Components/Galeria/Carousel/index.js
+++ src/Components/Galeria/Carousel/index.js
@@ -19,54 +19,186 @@ export const Carrossel = () => {
             id: 2,
             produto: "Perfume",
             descricao: "Perfume descrição",
-            preco: "R$ 200,00"
+            preco: "R$ 30,00"
         },
         {
             id: 3,
             produto: "Creme para o corpo",
             descricao: "creme descriçao",
-            preco: "R$ 30,00"
+            preco: "R$ 40,00"
+        },
+        {
+            id: 4,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 50,00"
+        },
+        {
+            id: 5,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 60,00"
+        },
+        {
+            id: 6,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 70,00"
+        },
+        {
+            id: 7,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 80,00"
+        },
+        {
+            id: 8,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 90,00"
+        },
+        {
+            id: 9,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 100,00"
+        },
+        {
+            id: 10,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 110,00"
+        },
+        {
+            id: 11,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 120,00"
+        },
+        {
+            id: 12,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 130,00"
+        },
+        {
+            id: 13,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 140,00"
+        },
+        {
+            id: 14,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 150,00"
+        },
+        {
+            id: 15,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 160,00"
+        },
+        {
+            id: 16,
+            produto: "Shampoo",
+            descricao: "Shampoo descrição",
+            preco: "R$ 170,00"
+        },
+        {
+            id: 17,
+            produto: "Perfume",
+            descricao: "Perfume descrição",
+            preco: "R$ 180,00"
+        },
+        {
+            id: 18,
+            produto: "Creme para o corpo",
+            descricao: "creme descriçao",
+            preco: "R$ 190,00"
         }
     ]
+    var produtos1=[];
+    var produtos2=[];
+    var produtos3=[];
+    var produtos4=[];
+    
+    function primeirosCards() {
+        for (var i=0; i<6; i++){
 
+            if(i<3){
+                produtos1.push(produtos[i])
+            }
+            if(i>2 && i<6){
+                produtos2.push(produtos[i])
+            }
+        }
+    }
+    
+
+    function sortearCards() {
+        for (var i=0; i<6; i++){
+            var s = getRandomArbitrary(produtos.length, 1);
+            if(i<3){
+                produtos3.push(produtos[s])
+            }
+            if(i>2 && i<6){
+                produtos4.push(produtos[s])
+            }
+        }
+        
+    }
+    
+    function getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    primeirosCards();
+
+    sortearCards();
+    
     return (
         <div>
-            <Carousel>
-                <Carousel.Item>
+            <Carousel >
+                <Carousel.Item >
                     <div className="container-card">
-                        {produtos.map(({ id, produto, descricao, preco }) => {
+                        {produtos1.map(({ id, produto, descricao, preco }) => {
                             return (
                                 <Item id={id} produto={produto} descricao={descricao} preco={preco} />
                             )
                         })}
                     </div>
                 </Carousel.Item>
-
-                <Carousel.Item>
+                <Carousel.Item >
                     <div className="container-card">
-                        <Item />
-                        <Item />
-                        <Item />
+                        {produtos2.map(({ id, produto, descricao, preco }) => {
+                            return (
+                                <Item id={id} produto={produto} descricao={descricao} preco={preco} />
+                            )
+                        })}
                     </div>
                 </Carousel.Item>
             </Carousel>
 
             {/* CARROSSEL 2 */}
 
-            <Carousel>
-                <Carousel.Item>
+            <Carousel >
+                <Carousel.Item >
                     <div className="container-card">
-                        <Item />
-                        <Item />
-                        <Item />
+                        {produtos3.map(({ id, produto, descricao, preco }) => {
+                            return (
+                                <Item id={id} produto={produto} descricao={descricao} preco={preco} />
+                            )
+                        })}
                     </div>
                 </Carousel.Item>
-
-                <Carousel.Item>
+                <Carousel.Item >
                     <div className="container-card">
-                        <Item />
-                        <Item />
-                        <Item />
+                        {produtos4.map(({ id, produto, descricao, preco }) => {
+                            return (
+                                <Item id={id} produto={produto} descricao={descricao} preco={preco} />
+                            )
+                        })}
                     </div>
                 </Carousel.Item>
             </Carousel>
