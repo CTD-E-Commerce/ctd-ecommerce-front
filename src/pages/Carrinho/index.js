61db09f129e2f609f5b13f1c6837c34a600ac4aa
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-12-08 14:19:03 -0300

    atualiza layout carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -9,53 +9,48 @@ const Carrinho = () => {
     return (
         <>
             <Container className="container-carrinho">
-                <Row>
-                <Col xl={8} className="card-carrinho">
-                            <div className="col-align-items-center">
-                                <img
-                                    src="https://amarecuidar.s3.sa-east-1.amazonaws.com/hair_care/condicionador-kerastase.jpg"
-                                    className="img-cart"
-                                    alt="#"
-                                />
-                            </div>
-                            
-                           
-                                <div className="col-align-items-center">
-                                    <span className="subtitulo-itens">Produto</span>
-                                    <span>Creme Elixir</span>
-                                </div>
-                          
-                
-                              <div className="col-align-items-center">
-                                    <span className="subtitulo-itens">Valor</span>
-                                    <span>R$ 159,00</span> 
-                              </div>
-                    
-                        
-                             <div className="col-align-items-center">
-                                    <span className="subtitulo-itens">Quantidade</span>
-                                    <Form.Select aria-label="Default select example" style={{width: "60px"}}>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
-                                        <option value="5">5</option>
-                                    </Form.Select>
-                             </div>
-                       
-                      
-                              <div className="col-align-items-center">
-                                    <span className="subtitulo-itens">Total</span> 
-                                    <span>R$ 159,00</span> 
-                              </div>
-                      
-    
-                               <div className="col-align-items-center"> <ImBin /></div>
+                <Row className="col-gap">
+                    <Col md={7}>
+                        {/* row com cada card de produto */}
+                        <Row className="card-carrinho row align-items-center">
+                            <Col className="col-align-items-center" sm={2} xs={12}>
+                            <img
+                                src="https://amarecuidar.s3.sa-east-1.amazonaws.com/hair_care/condicionador-kerastase.jpg"
+                                className="img-cart"
+                                alt="#"
+                            />
+                            </Col>
+                            <Col className="col-align-items-center"  sm={2}  xs={12}>
+                                <span className="subtitulo-itens">Produto</span>
+                                <span>Creme Elixir</span>
+                            </Col>
+                            <Col className="col-align-items-center">
+                                <span className="subtitulo-itens">Valor</span>
+                                <span>R$ 159,00</span> 
+                            </Col>
+                            <Col className="col-align-items-center"  sm={2}  xs={12}>
+                                <span className="subtitulo-itens">Quantidade</span>
+                                <Form.Select aria-label="Default select example" style={{width: "60px"}}>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                </Form.Select>
+                            </Col>
+                            <Col className="col-align-items-center"  sm={2}  xs={12}>
+                                <span className="subtitulo-itens">Total</span> 
+                                <span>R$ 159,00</span> 
+                            </Col>
+                            <Col className="col-align-items-center" sm={2}  xs={12}>
+                                <ImBin />
+                            </Col>
+                        </Row>
                     </Col>
 
 
                     {/* CARD DO RESUMO E FECHAMENTO DO PEDIDO */}
-                    <Col xl={4} >
+                    <Col md={4} >
                         <div className="carrinho p-3 mb-5">
                             <h1>Resumo do Pedido</h1>
                             <Table responsive="sm" size="sm">
