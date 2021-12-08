abd7cc1237f1f58156c3e5a386b4f19f7b51c5e3
Dâmares Bortolucci de Sousa <damares_sp78@yahoo.com.br>
2021-12-08 11:31:36 -0300

    atualizao pag carrinho

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -11,6 +11,7 @@ const Carrinho = () => {
             <Container className="container-carrinho">
                 <Row>
                     <Col xl={8}>
+                        {/* row com cada card de produto */}
                         <Row className="card-carrinho row align-items-center">
                             <Col className="col-align-items-center">
                             <img
@@ -33,49 +34,30 @@ const Carrinho = () => {
                             <Col className="col-align-items-center">R$ 159,00</Col>
                             <Col className="col-align-items-center"><ImBin /></Col>
                         </Row>
-                        <Row className="card-carrinho row align-items-center">
-                            <Col className="col-align-items-center">
-                            <img
-                                src="https://amarecuidar.s3.sa-east-1.amazonaws.com/hair_care/condicionador-kerastase.jpg"
-                                className="img-cart"
-                                alt="imagem do produto"
-                            />
-                            </Col>
-                            <Col className="col-align-items-center"><span>Creme Elixir</span></Col>
-                            <Col>R$ 159,00</Col>
-                            <Col className="col-align-items-center">
-                                <Form.Select aria-label="Default select example" >
-                                    <option value="1">1</option>
-                                    <option value="2">2</option>
-                                    <option value="3">3</option>
-                                    <option value="3">4</option>
-                                    <option value="3">5</option>
-                                </Form.Select>
-                            </Col>
-                            <Col className="col-align-items-center">R$ 159,00</Col>
-                            <Col className="col-align-items-center"><ImBin /></Col>
-                        </Row>
                     </Col>
+
+
+                    {/* CARD DO RESUMO E FECHAMENTO DO PEDIDO */}
                     <Col xl={4} >
                         <div className="carrinho shadow p-3 mb-5">
                             <h1>Resumo do Pedido</h1>
-                                <Table responsive="sm" size="sm">
-                                    <tbody>
-                                      <tr>
-                                        <td colSpan="2">Subtotal</td>
-                                        <td>R$ </td>
-                                      </tr>
-                                      <tr>
-                                        <td colSpan="2">Frete</td>
-                                        <td>R$ </td>
-                                      </tr>
-                                      <tr>
-                                        <td colSpan="2">Total</td>
-                                        <td>R$ </td>
-                                      </tr>
-                                    </tbody>
-                                </Table>
-                                <button className="cart-btn">Fechar Pedido</button>
+                            <Table responsive="sm" size="sm">
+                                <tbody>
+                                  <tr>
+                                    <td colSpan="2">Subtotal</td>
+                                    <td>R$ </td>
+                                  </tr>
+                                  <tr>
+                                    <td colSpan="2">Frete</td>
+                                    <td>R$ </td>
+                                  </tr>
+                                  <tr>
+                                    <td colSpan="2" style={{fontWeight:"bold"}}>Total</td>
+                                    <td style={{fontWeight:"bold"}}>R$ </td>
+                                  </tr>
+                                </tbody>
+                            </Table>
+                            <button className="cart-btn">Fechar Pedido</button>
                         </div>
                     </Col>
                 </Row>
